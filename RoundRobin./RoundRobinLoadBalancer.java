ecfaef461d56c5d991593abc5755a5b31d1d164a
kavyalingutla <91370631+kavyalingutla@users.noreply.github.com>
2023-11-30 16:37:07 +0530

    Update RoundRobinLoadBalancer.java

--- RoundRobin./RoundRobinLoadBalancer.java
+++ RoundRobin./RoundRobinLoadBalancer.java
@@ -1,1 +1,26 @@
+package org.cloudbus.cloudsim.examples;
+
+
+import java.util.List;
+
+public class RoundRobinLoadBalancer {
+    private int currentIndex;
+
+    public RoundRobinLoadBalancer() {
+        currentIndex = 0;
+    }
+
+    public synchronized int getNextVmId(List<Integer> vmIds) {
+        if (vmIds.isEmpty()) {
+            throw new IllegalArgumentException("List of VM IDs is empty");
+        }
+
+        int nextVmId = vmIds.get(currentIndex);
+        currentIndex = (currentIndex + 1) % vmIds.size();
+        return nextVmId;
+    }
+
+   
+}
+
 
